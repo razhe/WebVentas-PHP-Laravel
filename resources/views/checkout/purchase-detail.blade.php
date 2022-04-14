1f7497615d01a1fc815ae8225cda01efc086c3f2
razhe <gustavo.acu.rivas@gmail.com>
2022-04-13 20:23:39 -0400

    diseños basicos del detalle de la orden

--- resources/views/checkout/purchase-detail.blade.php
+++ resources/views/checkout/purchase-detail.blade.php
@@ -3,7 +3,7 @@
 
 @section('CSS')
     <!--CSS-->
-        <link rel="stylesheet" href="{{url('')}}">
+        <link rel="stylesheet" href="{{url('static/css/purchase-detail.css')}}">
 @endsection
 
 @section('content')
@@ -27,52 +27,60 @@
 </div>
 @endif
 <section class="checkout-content">
-    <table>
-        <thead>
-            <tr>
-                <th>Código</th>
-                <th>Nombre del artículo</th>
-                <th>Precio</th>
-                <th>Cantidad</th>
-                <th>% Descuento</th>
-                <th>Subtotal</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($i = 0; $i < sizeOf($productos); $i++)
+    <div class="box-title">
+        <h4>Detalles de la compra</h4>
+        <div class="hline"></div>
+    </div>
+    <div class="box-tabla-compras">
+        <table class="table-detail">
+            <thead>
                 <tr>
-                    <td>-</td>
-                    <td>{{$productos[$i]['nombre']}}</td>
-                    <td>{{$productos[$i]['precio']}}</td>
-                    <td>{{$productos[$i]['cantidad']}}</td>
-                    <td>{{$productos[$i]['descuento']}} %</td>
-                    <td>{{$productos[$i]['subtotal']}}</td>
+                    <th>Código</th>
+                    <th>Nombre del artículo</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                    <th>Descuento</th>
+                    <th>Subtotal</th>
                 </tr>
-            @endfor
-        </tbody>
-    </table>
-
-    <table>
-        <tbody id="box-rows-totalCarrito">
-            <tr>
-                <td>Total Neto</td>
-                <td>{{$totales[0]['total_neto']}}</td>
-            </tr>
-            <tr>
-                <td>IVA{{Config::get('configuracion-global.iva')}}</td>
-                <td>{{$totales[0]['iva']}}</td>
-            </tr>
-            <tr>
-                <td>Total</td>
-                <td>{{$totales[0]['total']}}</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <a href="{{url('/profile')}}">Ver mis compras</a>
-    <a href="{{url('/products')}}">Seguir comprando</a>
+            </thead>
+            <tbody>
+                @for ($i = 0; $i < sizeOf($productos); $i++)
+                    <tr>
+                        <td>-</td>
+                        <td>{{$productos[$i]['nombre']}}</td>
+                        <td>{{Config::get('configuracion-global.currency').$productos[$i]['precio']}}</td>
+                        <td>{{$productos[$i]['cantidad']}}</td>
+                        <td>{{$productos[$i]['descuento']}} %</td>
+                        <td>{{Config::get('configuracion-global.currency').$productos[$i]['subtotal']}}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
+    <div class="box-tabla-summary">
+        <table class="table-summary">
+            <tbody id="box-rows-totalCarrito">
+                <tr>
+                    <td><strong>Total Neto</strong></td>
+                    <td>{{Config::get('configuracion-global.currency').$totales[0]['total_neto']}}</td>
+                </tr>
+                <tr>
+                    <td><strong>IVA ({{Config::get('configuracion-global.iva')}}%)</strong></td>
+                    <td>{{Config::get('configuracion-global.currency').$totales[0]['iva']}}</td>
+                </tr>
+                <tr>
+                    <td><strong>Total</strong></td>
+                    <td>{{Config::get('configuracion-global.currency').$totales[0]['total']}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="opts-purchase-detail">
+        <a class="" href="{{url('/profile')}}">Ver mis compras</a>
+        <a class="" href="{{url('/products')}}">Seguir comprando <i class="fa-solid fa-arrow-right-long"></i></a>
+    </div>
 </section>
 @endsection
 @section('JS')
-    <script src="{{url('')}}"></script>
+    <script src="{{url('static/js/purchase-detail.js')}}"></script>
 @endsection
